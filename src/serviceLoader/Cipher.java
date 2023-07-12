d203f69070cc27f11047f10d8ac185bc4792af2d
Mamaafrica <k_vashalomidze@mail.ru>
2023-07-12 19:39:22 +0400

    final version

--- src/serviceLoader/Cipher.java
+++ src/serviceLoader/Cipher.java
@@ -1,7 +1,35 @@
 package serviceLoader;
 
+/**
+ * @author Konstantine Vashalomidze
+ * <description>
+ *     Interface to group different cipher algorithms together
+ * </description>
+ */
 public interface Cipher {
+    /**
+     * <description>
+     *     encrypts array of byte
+     * </description>
+     * @param source Source array to encrypt
+     * @param key Key for encryption
+     * @return Encrypted source
+     */
     byte[] encrypt(byte[] source, byte[] key);
+
+    /**
+     * <description>
+     *      Decrypts array of byte
+     * </description>
+     * @param source Source array to decrypt
+     * @param key Key which was used for encryption
+     * @return Decrypted source
+     */
     byte[] decrypt(byte[] source, byte[] key);
+
+    /**
+     * Determines the "strength" of the algorithm
+     * @return Strength value, always positive
+     */
     int strength();
 }
--- src/serviceLoader/impl/CaesarCipher.java
+++ src/serviceLoader/impl/CaesarCipher.java
@@ -6,14 +6,7 @@ import serviceLoader.Cipher;
 public class CaesarCipher implements Cipher
 {
 
-    /**
-     * <description>
-     *     encrypts array of byte
-     * </description>
-     * @param source Source array to encrypt
-     * @param key Key for encryption
-     * @return Encrypted source
-     */
+
     @Override
     public byte[] encrypt(byte[] source, byte[] key)
     {
@@ -26,14 +19,7 @@ public class CaesarCipher implements Cipher
 
     }
 
-    /**
-     * <description>
-     *      Decrypts array of byte
-     * </description>
-     * @param source Source array to decrypt
-     * @param key Key which was used for encryption
-     * @return Decrypted source
-     */
+
     @Override
     public byte[] decrypt(byte[] source, byte[] key)
     {
@@ -45,10 +31,7 @@ public class CaesarCipher implements Cipher
         return encrypt(source, new byte[]{ (byte) (-key[0]) }); // This is better. Avoid complexity.
     }
 
-    /**
-     * Determines the "strength" of the algorithm
-     * @return Strength value, positive
-     */
+
     @Override
     public int strength()
     {
